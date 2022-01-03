7c27f7e3c75e1d3c8352146a219328fcebf3c3b7
sara johansson <sara.johanss0n@hotmail.com>
2022-01-03 15:29:09 +0100

    singelton db

--- Database.cs
+++ Database.cs
@@ -3,33 +3,63 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
 
 namespace Projekt_Fight_Game
 {
-    class SingletonDB
+    public sealed class SingletonDB
     {
-        
-         /* private static SingletonDB dbInstance;
-            private readonly SqlConnection conn = new SqlConnection(@"");
+        private static SingletonDB dbInstance = null;
+        private static readonly object padlock = new object();
+        private string conString = (@"Data Source=LAPTOP-EQLFMT99\SQLEXPRESS;Initial Catalog=DBFightGame;Integrated Security=True");
 
-            private SingletonDB() //konstruktor
+        private SingletonDB()
+        {
+            DataTable dt = new DataTable();
+
+            using (SqlConnection Conn = new SqlConnection(conString))
             {
+                Conn.Open();
+                if (Conn.State == System.Data.ConnectionState.Open)
+                {
+                    MessageBox.Show("connected");
+
+                }
+
+                string sql = "SELECT * FROM tblUser";
+
+                using (SqlCommand cmd = new SqlCommand(sql, Conn))
+                {
+
+                    SqlDataAdapter da = new SqlDataAdapter(cmd);
+                    da.Fill(dt);
+
+
+                }
             }
 
-            public static SingletonDB getDbInstance()
+        }
+
+        public static SingletonDB Instance
+        {
+            get
             {
                 if (dbInstance == null)
                 {
-                    dbInstance = new ConnSingleton();
+                    lock (padlock)
+                    {
+                        if (dbInstance == null)
+                        {
+                            dbInstance = new SingletonDB();
+                        }
+                    }
                 }
                 return dbInstance;
             }
+        }
 
-            public SqlConnection GetDBConnection()
-            {
-                
-            } */
-
-        
     }
 }
