0c9e06067ebcd353ad6feefaa89996f8874cec1e
Michael Lennheden <michael@lennheden.com>
2022-01-03 09:29:10 +0100

    Added Character class and Character decorator.

---
+++ Character/PlayerRanger.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekt_Fight_Game
+{
+    internal class PlayerRanger:Character
+    {
+        public string UserClass()
+        {
+            return "Ranger";
+        }
+
+        public string Gender()
+        {
+            return "Female";
+        }
+    }
+}
